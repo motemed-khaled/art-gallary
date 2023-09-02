91c4cac29c412fe7fb34dc9a1d7c4c48c9ab07fb
motemed khaled <motemedkhaled5@gmail.com>
2023-09-02 21:41:24 +0300

    cart service

--- server/src/routes/mount.routes.ts
+++ server/src/routes/mount.routes.ts
@@ -4,6 +4,7 @@ import { router as authRoutes } from "./auth.routes";
 import { router as categoryRoutes } from "./category.routes";
 import { router as productRoutes } from "./product.routes";
 import { router as reviewRoutes } from "./review.routes";
+import { router as cartRoutes } from "./cart.routes";
 
 
 export const mountRoutes = (app: Application) => {
@@ -12,4 +13,5 @@ export const mountRoutes = (app: Application) => {
     app.use("/api/v1/category", categoryRoutes);
     app.use("/api/v1/product", productRoutes);
     app.use("/api/v1/review", reviewRoutes);
-}
+    app.use("/api/v1/cart", cartRoutes);
+};
